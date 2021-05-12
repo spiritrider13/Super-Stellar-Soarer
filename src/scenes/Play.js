ce5a53945b89ece8af64534179e482d983525ccc
samhvik <samhvik@gmail.com>
2021-05-12 14:23:53 -0700

    forgot Play scene

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -18,6 +18,14 @@ class Play extends Phaser.Scene {
         //add some text labels
         this.sceneText = this.add.text(game.config.width/2, 20, 'PLAY', titleTextConfig).setOrigin(0.5);
         
+        // BACK BUTTON ***********************************************************************
+        const backButton = new Button(this, game.config.width/2, 1040);
+        this.add.existing(backButton);
+        this.backButtonText = this.add.text(game.config.width/2, 1000, 'BACK TO HOME', titleTextConfig).setOrigin(0.5);
+        backButton.setInteractive()
+        .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+            this.scene.start('homeScene');
+        })
 
     }
 
