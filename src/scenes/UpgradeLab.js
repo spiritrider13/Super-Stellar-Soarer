9cdf004ab204b6e3d5942d9813a1832a4dda42d4
Aria Altenburg <aria.altenburg@cox.net>
2021-06-02 14:45:48 -0700

    added temp upgrade backgrounds

--- src/scenes/UpgradeLab.js
+++ src/scenes/UpgradeLab.js
@@ -10,12 +10,17 @@ class UpgradeLab extends Phaser.Scene {
     }
 
     preload() {
-        
+        this.load.image('background', './assets/upgradeBackground.png');
+
 
 
     }
 
     create() {
+        // background
+        this.background = this.add.tileSprite(0,0,720,1080,'background').setOrigin(0,0);
+
+
         //add some text labels
         this.sceneText = this.add.text(game.config.width/2, 20, 'UPGRADE LAB', titleTextConfig).setOrigin(0.5);
 
--- src/scenes/UpgradeShip.js
+++ src/scenes/UpgradeShip.js
@@ -10,12 +10,17 @@ class UpgradeShip extends Phaser.Scene {
     }
 
     preload() {
+        this.load.image('background', './assets/upgradeBackground.png');
+
         
 
 
     }
 
     create() {
+        // background
+        this.background = this.add.tileSprite(0,0,720,1080,'background').setOrigin(0,0);
+
         //add some text labels
         this.sceneText = this.add.text(game.config.width/2, 20, 'UPGRADE SHIP', titleTextConfig).setOrigin(0.5);
 
