ed42d6c319d588dc1aa8b4da95ad2aee877f131a
Aria Altenburg <aria.altenburg@gmail.com>
2021-06-01 19:42:36 -0700

    update roles in credits

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -17,23 +17,25 @@ class Credits extends Phaser.Scene {
         this.background = this.add.tileSprite(0,0,720,1080,'background').setOrigin(0,0);
 
         // show menu text
-        this.add.text(game.config.width/2, game.config.height/6 - borderUISize - borderPadding, 'CREDITS', titleTextConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/4 - borderUISize - borderPadding, 'TEAM AJS', normalTextConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/4, 'Programming', titleTextConfig).setOrigin(0.5);  
-        this.add.text(game.config.width/2, game.config.height/2.1, 'Art & Design', titleTextConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/1.53, 'Music & Sound', titleTextConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, 175, 'CREDITS', titleTextConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, 250, 'TEAM AJS', titleTextConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, 355, 'Programming', titleTextConfig).setOrigin(0.5);  
+        this.add.text(game.config.width/2, 550, 'Art & Design', titleTextConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, 750, 'Music & Sound', titleTextConfig).setOrigin(0.5);
 
         // Programming
-        this.add.text(game.config.width/2, game.config.height/3.25, 'Aria Altenburg', normalTextConfig).setOrigin(0.5);  
-        this.add.text(game.config.width/2, game.config.height/2.85, 'Jason Lee', normalTextConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2.53, 'Sam Vik', normalTextConfig).setOrigin(0.5);        
+        this.add.text(game.config.width/2, 390, 'Sam Vik', normalTextConfig).setOrigin(0.5);  
+        this.add.text(game.config.width/2, 420, 'Jason Lee', normalTextConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, 450, 'Aria Altenburg', normalTextConfig).setOrigin(0.5);        
 
         // Art & Design
-        this.add.text(game.config.width/2, game.config.height/1.9, 'Aria Altenburg', normalTextConfig).setOrigin(0.5);  
-        this.add.text(game.config.width/2, game.config.height/1.75, 'Jason Lee', normalTextConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, 585, 'Jason Lee', normalTextConfig).setOrigin(0.5);  
+        this.add.text(game.config.width/2, 615, 'Aria Altenburg', normalTextConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, 645, 'Sam Vik', normalTextConfig).setOrigin(0.5);        
+
 
         // Music & Sound
-        this.add.text(game.config.width/2, game.config.height/1.41, 'Jason Lee', normalTextConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, 785, 'Jason Lee', normalTextConfig).setOrigin(0.5);
 
         // BACK BUTTON ***********************************************************************
         const backButton = new Button(this, game.config.width/2, 1040);
