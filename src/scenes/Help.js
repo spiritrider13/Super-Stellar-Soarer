feece2761d3c735e7b5fa0f4298f3a85db14d8da
Jason Lee <jlee668@ucsc.edu>
2021-06-07 14:36:44 -0700

    add more text to help scene

--- src/scenes/Help.js
+++ src/scenes/Help.js
@@ -18,7 +18,7 @@ class Help extends Phaser.Scene {
     create() {
         // background
         this.add.rectangle(0,0,game.config.width,game.config.height,0xe5d3b3).setOrigin(0,0);
-        this.gameControl = this.add.sprite(130, 300, 'controller').setOrigin(0,0);
+        this.gameControl = this.add.sprite(130, 500, 'controller').setOrigin(0,0);
 
         //add some text labels
         this.sceneText = this.add.text(game.config.width/2, 20, 'HOW TO PLAY', titleTextConfig).setOrigin(0.5);
@@ -37,5 +37,8 @@ class Help extends Phaser.Scene {
             'Press PLAY to be taken to the main screen,\nwhere you can navigate the different menus of the game.', normalTextConfig).setOrigin(0.5);
         this.helpText2 = this.add.text(game.config.width/2, 300, 
             'In the fuel crafting menu,you can select three different\nfuel components that will determine different stats of your rocket.', normalTextConfig).setOrigin(0.5);
+        this.helpText2 = this.add.text(game.config.width/2 + 30, 400, 
+            'In the upgrade shop,you can upgrade four different components such\nas wings, nose boosters, and fuel option. This will hugely impact \non your flight.', normalTextConfig).setOrigin(0.5);
+       this.helpText3 = this.add.text(game.config.width/+30, 450, 'During the flight, you need be cautious of falling obstacles\nand losing fuel.');
     }
 }
